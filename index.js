9e01b661dbd6c40300e53bed88dd4856e3e39dc5
Tabitha <flor.palida@yahoo.com>
2023-05-26 21:28:44 -0400

    completed assignment

---
+++ index.js
@@ -0,0 +1,57 @@
+function add() {
+}
+function subtract() {
+}
+function multiply() {
+}
+function divide() {
+    }
+function add() {
+    return "something";
+}
+function add(a, b) {
+    return "something";
+}
+function add(a, b) {
+    return a + b;
+    }
+function subtract(a, b) {
+    return a-b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function makeInt(n) {
+    return parseInt(`n`,10)
+}
+function preserveDecimal(n) {
+    parseFloat(`2.222`)
+}
+var n = 0;
+
+function increment(){
+
+  n++;
+  return n;
+}
+var i = 0;
+
+function increment(n){
+
+  n++;
+  return n;
+}
+
+i=increment(i);
+var i = 0;
+
+function decrement(n){
+
+  n--;
+  return n;
+}
+
+i=decrement(i);
